0697052255936c5d09befc30c4439ac3fa0c23ef
Viktor Lazarov <viktorlazarov@abv.bg>
2023-04-23 14:53:52 +0200

    Create 04previousDay.js

---
+++ 04previousDay.js
@@ -0,0 +1,10 @@
+function previousDay(year, month, day) {
+    let date = new Date(year, month - 1, day)
+    date.setDate(date.getDate() - 1)
+    console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+
+}
+
+previousDay(2016, 9, 30)
+previousDay(2015, 10, 1)
+previousDay(2015, 5, 10)
