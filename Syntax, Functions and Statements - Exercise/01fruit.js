ad75e143646f54727991bbdbf1f6c91df671721a
Viktor Lazarov <viktorlazarov@abv.bg>
2023-04-23 13:16:00 +0200

    Update 01fruit.js

--- Syntax, Functions and Statements - Exercise/01fruit.js
+++ Syntax, Functions and Statements - Exercise/01fruit.js
@@ -1,5 +1,9 @@
-function fruit(params) {
-    console.log(params);
+function fruit(fruit, weigth, price) {
+    let money = price * (weigth/1000);
+
+
+    console.log(`I need $${money.toFixed(2)} to buy ${(weigth/1000).toFixed(2)} kilograms ${fruit}.`);
 }
 
-fruit('sdfsdf');
+fruit('orange', 2500, 1.80);
+fruit('apple', 1563, 2.35);
